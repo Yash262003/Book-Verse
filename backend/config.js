34ff9aa498fc5062638f2c80bbe429604cc76745
navoday <navodayskaushal01@gmail.com>
2024-04-03 23:18:46 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://root:root@book-store-mern.h3rb94a.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
+
---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+//middleware for parsing request body
+app.use(express.json());
+
+app.use(cors());
+
+//middleware for handling CORS policy
+//option 1: allow all origins with default of cors(*)
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+//     );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to DB");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
